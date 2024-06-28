d10b5efe501e4de310b29988dfacc86ebe2bfb4f
kishor jawale <kishorjawale001@gmail.com>
2024-06-28 21:11:09 +0530

    Add files via upload

---
+++ INTRODUCTION/FloatDataType.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float f = 3.123515346745;
+    cout<<f;
+    return 0;
+}
---
+++ INTRODUCTION/findRemainder.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 19;
+    int b = 5;
+    int q = a/b;
+    int r = a - (b * q);
+    cout<<r<<endl;
+}
